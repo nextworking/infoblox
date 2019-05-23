c12cf4d8e3d65fb39ca8fd363a64a7d0f059f75a
bazzie <bas@wildemann.nl>
2019-05-23 06:40:47 +0200

    Added sendRequest and buildRequest

--- client.go
+++ client.go
@@ -2,10 +2,13 @@ package infoblox
 
 import (
 	"crypto/tls"
+	"fmt"
 	"golang.org/x/net/publicsuffix"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
+	"net/url"
+	"strings"
 )
 
 var (
@@ -27,14 +30,16 @@ type Client struct {
 	UseCookies bool
 }
 
+type APIResponse http.Response
+
 func NewClient(host, username, password string, sslVerify, useCookies bool) *Client {
 
-  var (
-	  req, _    = http.NewRequest("GET", host, nil)
-	  proxy, _  = http.ProxyFromEnvironment(req)
-	  transport *http.Transport
-	  tlsconfig *tls.Config
-  )
+	var (
+		req, _    = http.NewRequest("GET", host, nil)
+		proxy, _  = http.ProxyFromEnvironment(req)
+		transport *http.Transport
+		tlsconfig *tls.Config
+	)
 
 	tlsconfig = &tls.Config{
 		InsecureSkipVerify: !sslVerify,
@@ -68,4 +73,62 @@ func NewClient(host, username, password string, sslVerify, useCookies bool) *Cli
 
 	return client
 
+}
+
+func (c *Client) SendRequest(method, urlStr, body string, head map[string]string) (resp *APIResponse, err error) {
+	// log.Printf("%s %s  payload: %s\n", method, urlStr, body)
+	req, err := c.buildRequest(method, urlStr, body, head)
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %v", err)
+	}
+	var r *http.Response
+	if !c.UseCookies {
+		// Go right to basic auth if we arent using cookies
+		req.SetBasicAuth(c.Username, c.Password)
+	}
+
+	r, err = c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error executing request: %v", err)
+	}
+	if r.StatusCode == 401 && c.UseCookies { // don't bother re-sending if we aren't using cookies
+		log.Printf("Re-sending request with basic auth after 401")
+		// Re-build request
+		req, err = c.buildRequest(method, urlStr, body, head)
+		if err != nil {
+			return nil, fmt.Errorf("error re-creating request: %v", err)
+		}
+		// Set basic auth
+		req.SetBasicAuth(c.Username, c.Password)
+		// Resend request
+		r, err = c.HTTPClient.Do(req)
+	}
+	resp = (*APIResponse)(r)
+	return
+}
+
+// build a new http request from this client
+func (c *Client) buildRequest(method, urlStr, body string, head map[string]string) (*http.Request, error) {
+	var req *http.Request
+	var err error
+	if body == "" {
+		req, err = http.NewRequest(method, urlStr, nil)
+	} else {
+		b := strings.NewReader(body)
+		req, err = http.NewRequest(method, urlStr, b)
+	}
+	if err != nil {
+		return nil, err
+	}
+	if !strings.HasPrefix(urlStr, "http") {
+		u := fmt.Sprintf("%v%v", c.Host, urlStr)
+		req.URL, err = url.Parse(u)
+		if err != nil {
+			return nil, err
+		}
+	}
+	for k, v := range head {
+		req.Header.Set(k, v)
+	}
+	return req, err
 }
